b18d7fb71cfc5dc23d362958c03ecb215a67b4fe
1Parksanghyun <nks168810@gmail.com>
2025-06-01 13:35:41 +0900

    25/06/01 스킬 정보 삽입 및 1차 디자인

--- Wiki/InfoPage/LoadNikkeData.js
+++ Wiki/InfoPage/LoadNikkeData.js
@@ -1,2 +1,35 @@
+//URL에 있는 캐릭터 이름 가져오기
 const NikkeName = window.location.pathname.split('/')[1];
-console.log(NikkeName);
+const NikkeProfileBox = document.getElementById("NikkeProfileBox");
+
+fetch('../MainHome/NikkeList.json')
+    .then(res => res.json())
+    .then(data => {
+        for (let index of data) {
+            if (index.dataname != NikkeName) {
+                console.log(`Not ${NikkeName}`);
+                continue;
+            }
+            else {
+                document.getElementById("NikkeName").innerText = index.name;
+                //스탠딩 이미지 삽입
+                document.getElementById("NikkeStandImg").src = `/InfoPage/InfoPageImage/${index.dataname}Stand.png`;
+                InsertSkillInfo();
+                break;
+            }
+        }
+    }
+    )
+
+function InsertSkillInfo() {
+    fetch('/InfoPage/SkillData.json')
+        .then(res => res.json())
+        .then(data => {
+            let index = 0;
+            for (let currentskill of Object.entries(data[`${NikkeName}`])) {
+                document.getElementsByClassName('SkillName')[index].innerText = currentskill[0];
+                document.getElementsByClassName('SkillEffect')[index++].innerText = currentskill[1];
+            }
+        }
+        )
+}
