72bb8ec2adc068c9155b1517cceccd17b6cecc04
1Parksanghyun <nks168810@gmail.com>
2025-06-08 02:29:07 +0900

    25/06/08 태그 중복 선택 불가능

--- Wiki/MainHome/Tags/SelectedTagList.js
+++ Wiki/MainHome/Tags/SelectedTagList.js
@@ -1,18 +1,21 @@
 function SelectTag(SelectedTagName, SelectedTagCode) {
-    fetch('./Tags/TagValueList.json')
-        .then(res => res.json())
-        .then(data => {
-            console.log(SelectedTagName, SelectedTagCode);
 
-            const SelectedTag = document.createElement('button');
-            SelectedTag.className = 'SelectedTag';
-            SelectedTag.innerText = SelectedTagName;
-            SelectedTag.dataset.name = SelectedTagCode;
-            SelectedTag.onclick = () => {
-                document.getElementById('SelectedTagList').removeChild(SelectedTag);
-            }
+    if (IsSelected(SelectedTagCode)) {
+        const SelectedTag = document.createElement('button');
+        SelectedTag.className = 'SelectedTag';
+        SelectedTag.innerText = SelectedTagName;
+        SelectedTag.dataset.name = SelectedTagCode;
+        SelectedTag.onclick = () => {
+            document.getElementById('SelectedTags').removeChild(SelectedTag);
+        }
+        document.getElementById('SelectedTags').appendChild(SelectedTag);
+    }
 
-            document.getElementById('SelectedTagList').appendChild(SelectedTag);
-        })
+}
 
+function IsSelected(SelectedTagCode) {
+    for (let index of document.getElementById('SelectedTags').children) {
+        if (SelectedTagCode == index.dataset.name) return 0;
+    }
+    return 1;
 }
